2c948e44ed9fb55e87abe81eba1e025b913f5f04
sumanchalki <suman.abc@gmail.com>
2019-06-22 16:09:11 +0530

    Documentations updated.

--- client.js
+++ client.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
+  // Loop through all the input boxes and add enter key press event listener.
+  // After the message is entered, make the textbox blank again.
   let input_nodes = document.querySelectorAll('input[type="text"]');
   if (input_nodes.length) {
     input_nodes.forEach(function(el_input) {
       el_input.addEventListener('keypress', function(e) {
+        // Enter key press.
         if (e.keyCode == 13) {
           let msg = el_input.value;
           switch(el_input.getAttribute('id')) {
--- server.js
+++ server.js
@@ -3,20 +3,23 @@ const uuid = require('uuid');
 const wss = new WebSocket.Server({port: 2000});
 
 wss.on('connection', function (ws) {
+  // Unique id is assigned to each client after the connection is made.
   ws.id = uuid.v4();
 
-  console.log(ws.id);
   wss.clients.forEach(function each(client) {
       console.log('Client.ID: ' + client.id);
   });
 
+  // Send the event back to client so it can display a new user is added.
   ws.send(JSON.stringify({type:'new_user', text: 'Anonymous', date: Date.now()}));
 
+  // Also send a broadcast message so other users can get notified.
   broadCastThis({type:'public_msg', text: 'Someone joined!', date: Date.now()});
 
   ws.on('message', function (message) {
     let messageParsed = JSON.parse(message);
     console.log(messageParsed);
+    // Check the message type, if it is public msg or username is updated, broadcast it.
     if (messageParsed.type === 'private_msg') {
       ws.send(message);
     }
@@ -26,6 +29,7 @@ wss.on('connection', function (ws) {
   });
 });
 
+// Broadcast this message by sending it to all the clients.
 function broadCastThis(message) {
   wss.clients.forEach(function each(client) {
     if (client.readyState === WebSocket.OPEN) {
